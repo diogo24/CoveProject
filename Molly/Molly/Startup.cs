7e8d62aa577e7311ed8fa4bb330fce4a1d2b0385
diogo24 <diocar24@hotmail.com>
2015-07-22 21:34:15 +0100

    Molly MVC Project

---
+++ Molly/Molly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Molly.Startup))]
+namespace Molly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
