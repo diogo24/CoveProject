32131a42f2197839d5d60fc53c31b3f6ba7de33b
diogo24 <diocar24@hotmail.com>
2015-07-22 22:38:16 +0100

    1) Add nugets Babylonjs and hand.js. 2) babylonjs has example

---
+++ Molly/Molly/Controllers/BabylonJSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Molly.Controllers
+{
+    public class BabylonJSController : Controller
+    {
+        // GET: BabylonJS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
